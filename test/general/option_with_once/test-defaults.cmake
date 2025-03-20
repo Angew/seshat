42302d5c6b81d20da5e24730b5d6b8d8a9308bdb
Petr Kmoch <petr.kmoch@gmail.com>
2025-03-20 09:50:44 +0100

    WIP on testing option_with_once()

---
+++ test/general/option_with_once/test-defaults.cmake
@@ -0,0 +1,12 @@
+# Copyright 2025 Petr Kmoch
+# Licensed under the Boost Software License Version 1.0
+# See accompanying file LICENSE or http://www.boost.org/LICENSE_1_0.txt
+
+
+execute_process(
+	# configure the project
+)
+
+execute_process(
+	# inspect it
+)
