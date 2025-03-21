3a6293f786b1b2d3849f20dc67e37910350b8c53
Petr Kmoch <petr.kmoch@gmail.com>
2025-03-21 09:23:19 +0100

    WIP on test of option_with_once()

--- test/general/option_with_once/test-defaults.cmake
+++ test/general/option_with_once/test-defaults.cmake
@@ -3,8 +3,18 @@
 # See accompanying file LICENSE or http://www.boost.org/LICENSE_1_0.txt
 
 
+# Arguments:
+# Generator
+# BinaryDir
+# ArchitectureArg [optional]
+# ToolsetArg [optional]
+
+
 execute_process(
-	# configure the project
+	COMMAND ${CMAKE_COMMAND}
+		-S ${CMAKE_CURRENT_LIST_DIR}/defaults -B ${BinaryDir}
+		-G ${Generator} ${ArchitectureArg} ${ToolsetArg}
+		--fresh
 )
 
 execute_process(
